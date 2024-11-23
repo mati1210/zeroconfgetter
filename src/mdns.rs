27b5488983c1627ad2f4c285454013d4f5b7a7ef
mat1210 <matheuz1210@gmail.com>
2024-11-22 21:39:12 -0300

    prefer_ipv6

--- src/mdns.rs
+++ src/mdns.rs
@@ -7,17 +7,20 @@ use tokio::spawn;
 use zeroconf_tokio::{prelude::*, MdnsBrowser, MdnsBrowserAsync, ServiceDiscovery, ServiceType};
 
 pub async fn listener(hosts: Hosts) {
+    let prefer_ipv6 = std::env::var_os("PREFER_IPV6").is_some();
+
     let service = ServiceType::new("ssh", "tcp").unwrap();
     let mut browser = crate::die!({MdnsBrowserAsync::new(MdnsBrowser::new(service)) } "failed creating mdns browser! {err}");
     crate::die!( {browser.start().await } "failed starting mdns browser! {err}");
     while let Some(Ok(discovery)) = browser.next().await {
         let _hosts = Arc::clone(&hosts);
-        spawn(async move { handler(discovery, _hosts).await });
+        spawn(async move { handler(discovery, _hosts, prefer_ipv6).await });
     }
 }
 
-async fn handler(service: ServiceDiscovery, hosts: Hosts) {
-    if service.address() == "127.0.0.1" || service.address().contains(':') {
+async fn handler(service: ServiceDiscovery, hosts: Hosts, prefer_ipv6: bool) {
+    let address = service.address();
+    if address == "127.0.0.1" || address.contains(if prefer_ipv6 { '.' } else { ':' }) {
         return;
     }
 
@@ -30,11 +33,11 @@ async fn handler(service: ServiceDiscovery, hosts: Hosts) {
     };
 
     let ro_lock = hosts.read().await;
-    if ro_lock.get(&host) == Some(service.address()) {
+    if ro_lock.get(&host) == Some(address) {
         return;
     }
     drop(ro_lock);
 
     let mut rw_lock = hosts.write().await;
-    rw_lock.insert(host, service.address().clone());
+    rw_lock.insert(host, address.clone());
 }
